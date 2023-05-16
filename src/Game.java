0850838f5a40d3c8906ae9a7d26ce0416ca13857
MateusNunes <marcelino.m.nunes@gmail.com>
2023-05-16 14:45:28 -0400

    Now the player moves

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
-public class Game extends Canvas implements Runnable
+public class Game extends Canvas implements Runnable, KeyListener
 {
 
     public static int WIDTH = 480, HEIGHT = 480;
+    public Player player;
 
     public void tick()
+    {
+        player.tick();
+    }
+
+    public void render()
     {
         BufferStrategy bs = this.getBufferStrategy();
 
@@ -20,19 +28,18 @@ public class Game extends Canvas implements Runnable
         g.setColor(Color.BLACK);
         g.fillRect(0,0, WIDTH, HEIGHT);
 
-        g.setColor(Color.RED);
-        g.fillRect(10, 10, 50, 50);
+        player.render(g);
+
+
         bs.show();
-    }
-
-    public void render()
-    {
-
     }
 
     public Game()
     {
+        this.addKeyListener(this);
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        player = new Player(0,0);
     }
 
     public static void main(String[] args)
@@ -54,20 +61,67 @@ public class Game extends Canvas implements Runnable
     @Override
     public void run()
     {
-        while (true)
+        do
         {
             tick();
             render();
             try
             {
-                Thread.sleep(1000/60);
-            } catch (InterruptedException e){
+                Thread.sleep(1000 / 60);
+            } catch (InterruptedException e)
+            {
                 e.printStackTrace();
             }
 
+        } while (true);
+
+
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+        {
+            player.right = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+        {
+            player.left = true;
         }
 
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+        {
+            player.up = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+        {
+            player.down = true;
+        }
+    }
 
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+        {
+            player.right = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+        {
+            player.left = false;
+        }
 
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+        {
+            player.up = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+        {
+            player.down = false;
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,12 +2,33 @@ import java.awt.*;
 
 public class Player extends Rectangle
 {
+    public boolean right, left, down, up;
+    public int spd = 4;
     public void tick()
     {
+        if(right)
+        {
+            x+=spd;
+        } else if (left)
+        {
+            x-=spd;
+        }
+
+        if(up)
+        {
+            y-=spd;
+        } else if (down)
+        {
+            y+=spd;
+        }
 
     }
 
     public void render(Graphics g)
+    {
+        g.setColor(Color.blue);
+        g.fillRect(x,y,width,height);
+    }
 
     public Player(int x, int y)
     {
